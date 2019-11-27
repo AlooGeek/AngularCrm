21004e4537679335419540a554972bdedc94e8eb
ahmedderbel <ahmed.derbel@esprit.tn>
2019-11-27 12:00:59 +0100

    Added Dashboard template

--- src/app/modules/back/dashboard/dashboard-routing.module.ts
+++ src/app/modules/back/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardCoreComponent } from './dashboard-core/dashboard-core.component';
+import { ProducttComponent } from './productt/productt.component';
 
 const routes: Routes = [
+  
+  {path: '',component: DashboardComponent , 
+  children:[ {path:'', component:DashboardCoreComponent},
+             {path:'product', component:ProducttComponent}
+
+
+
+]
+
+},
   
 ];
 
---
+++ src/app/modules/back/dashboard/productt/productt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productt',
+  templateUrl: './productt.component.html',
+  styleUrls: ['../dashboard.component.css']
+})
+export class ProducttComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
