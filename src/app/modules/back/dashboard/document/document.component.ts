29b8a77d5ad20c71d64ff1dd8bb7dd28f181ae55
Malek Rabei <Moukaaa@users.noreply.github.com>
2019-11-28 21:25:01 +0100

    doc shtar crud

--- src/app/modules/back/dashboard/document/document.component.ts
+++ src/app/modules/back/dashboard/document/document.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentService } from '../../../../core/services/document.service';
 
 @Component({
   selector: 'app-document',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document.component.css']
 })
 export class DocumentComponent implements OnInit {
-
-  constructor() { }
+ docs: Document[];
+ 
+  constructor(private docservice : DocumentService ) { }
 
   ngOnInit() {
+    return this.docservice.getDocument().subscribe(data=>this.docs=data);
   }
+
+  delete (id) {
+    if (window.confirm('Are you sure, you want to delete?')){
+      this.docservice.deleteDocument(id).subscribe(data => {
+        this.ngOnInit()
+      })
+    }
+  } 
+  
+  add (){
+    this.docservice.addDocument().subscribe(data => this.docs )
+
+
+
+  }
+  
 
 }
--- src/app/modules/back/dashboard/document/document.module.ts
+++ src/app/modules/back/dashboard/document/document.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DocumentRoutingModule } from './document-routing.module';
 import { DocumentComponent } from './document.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [DocumentComponent],
   imports: [
     CommonModule,
-    DocumentRoutingModule
+    DocumentRoutingModule,
+    HttpClientModule
   ]
 })
 export class DocumentModule { }
