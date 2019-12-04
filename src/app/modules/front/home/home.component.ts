38df39fbb5a8006fb551188dd14a90c695683771
ahmedderbel <ahmed.derbel@esprit.tn>
2019-12-04 11:08:51 +0100

    Discount front

--- src/app/modules/front/home/home.component.ts
+++ src/app/modules/front/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Discount } from 'src/app/core/models/Discount';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //src/assets/images/discount/${n}
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  ngOnInit() {
+  constructor(private api: ApiService,config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
   }
 
+  listdiscount:Discount[];
+
+  
+
+
+  ngOnInit() {
+    this.getAllDiscounts();
+  }
+
+
+  
+  getAllDiscounts(){
+    this.api.get("/discount").subscribe(res=>this.listdiscount=res);
+  }
 }
