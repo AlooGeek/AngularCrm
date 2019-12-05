e20b258f5acaf11834148824948c77c85e16763b
Malek Rabei <Moukaaa@users.noreply.github.com>
2019-12-05 20:35:40 +0100

    fix bill

--- src/app/modules/front/billing/billing.module.ts
+++ src/app/modules/front/billing/billing.module.ts
@@ -5,12 +5,14 @@ import { BillingRoutingModule } from './billing-routing.module';
 import { TrackComponent } from './track/track.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { BillingComponent } from './billing.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TrackComponent,CheckoutComponent,BillingComponent],
   imports: [
     CommonModule,
     BillingRoutingModule,
+    FormsModule,
   ],
 })
 export class BillingModule { }
