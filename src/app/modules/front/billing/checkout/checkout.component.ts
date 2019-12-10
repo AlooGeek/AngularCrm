6eaa40136f7661f37b70df07398b6b43572e1761
Triki-Ahmed <ahmed.triki@esprit.tn>
2019-12-10 15:36:40 +0100

    Fixation new price in ts

--- src/app/modules/front/billing/checkout/checkout.component.ts
+++ src/app/modules/front/billing/checkout/checkout.component.ts
@@ -65,7 +65,13 @@ calculTotal(){
   let i=0;
   let j=0
 for (j=0; j<this.listPanier.length;j++){
+
+  if (this.listPanier[i]['products']['newprice']!=0){
+    this.total = this.total+this.listPanier[i]['products']['newprice'];
+
+  }else{ 
   this.total = this.total+this.listPanier[i]['products']['unitPrice'];
+    }
   i++;
 }
 }
